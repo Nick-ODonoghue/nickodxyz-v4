939b0e336c717f67e3252ffc4a422c74b13e13f7
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-09-28 17:03:00 +0100

    ADDED: header component

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    // <header className=' z-50 relative container mx-auto'>
+    //   <div className=' fixed top-0 bg-red-300 bg-opacity-80 container border border-blue-500'>Hello</div>
+    // </header>
+    <header className='z-50 relative container mx-auto'>
+      <div className='fixed top-6 container bg-red-300 opacity-80 rounded-full px-4 py-2'>
+        <h1 className=' text-4xl font-medium'>NickOD.xyz</h1>
+      </div>
+    </header>
+  )
+}
