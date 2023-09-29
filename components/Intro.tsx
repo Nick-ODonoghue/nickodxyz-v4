63e3308e206e18d0b29c43a91e269f705ae920f3
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-09-29 11:43:26 +0100

    INSTALLED: tailwindcss/typography

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,6 +3,7 @@ import { SiteIntro } from "@/lib/interface"
 import { getIntroData } from "@/sanity/getSanityData"
 import { PortableText } from "@portabletext/react"
 import Image from "next/image"
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
 import { urlForImage } from "@/sanity/lib/image"
 
 const myPortableTextComponent = {
@@ -27,10 +28,20 @@ export default async function Intro() {
   if (!introData) return <div>Loading...</div>
 
   return (
-    <section className=' container mx-auto px-4 flex items-center text-center h-screen'>
+    <section className=' container mx-auto px-4 flex justify-center items-center text-center h-screen'>
       {introData.map((item) => (
-        <div key={item._id} className=' '>
-          <PortableText value={item.authorBio} components={myPortableTextComponent} />
+        <div key={item._id}>
+          <div className=' '>
+            <PortableText value={item.authorBio} components={myPortableTextComponent} />
+          </div>
+          <div className=' flex flex-row justify-center gap-6 text-6xl'>
+            <a href={item.socialLinks[0]} target='_blank'>
+              <AiFillLinkedin className='' />
+            </a>
+            <a href={item.socialLinks[1]} target='_blank'>
+              <AiFillGithub className='' />
+            </a>
+          </div>
         </div>
       ))}
     </section>
